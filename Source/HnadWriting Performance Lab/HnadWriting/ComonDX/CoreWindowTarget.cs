883ba581cd2845c4269b0681846dbb512026f199
Vulcan Lee <vulcan.lee@gmail.com>
2014-05-23 13:27:06 +0800

    HnadWriting Performance Lab 升級到 SharpDX 2.6,並修正相關錯誤

--- Source/HnadWriting Performance Lab/HnadWriting/ComonDX/CoreWindowTarget.cs
+++ Source/HnadWriting Performance Lab/HnadWriting/ComonDX/CoreWindowTarget.cs
@@ -1,13 +1,35 @@
-﻿using SharpDX;
+﻿// Copyright (c) 2010-2013 SharpDX - Alexandre Mutel
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SharpDX;
 using Windows.UI.Core;
 
-namespace HnadWriting.ComonDX
+namespace CommonDX
 {
+    using SharpDX.DXGI;
+    using HnadWriting.ComonDX;
+
     /// <summary>
     /// Target to render to a <see cref="CoreWindow"/>
     /// </summary>
@@ -53,7 +75,7 @@ namespace HnadWriting.ComonDX
         {
             // Creates a SwapChain from a CoreWindow pointer
             using (var comWindow = new ComObject(window))
-                return factory.CreateSwapChainForCoreWindow(device, comWindow, ref desc, null);
+                return new SwapChain1(factory, device, comWindow, ref desc);
         }
 
         private void window_SizeChanged(CoreWindow sender, WindowSizeChangedEventArgs args)
--- Source/HnadWriting Performance Lab/HnadWriting/ComonDX/SwapChainBackgroundPanelTarget.cs
+++ Source/HnadWriting Performance Lab/HnadWriting/ComonDX/SwapChainBackgroundPanelTarget.cs
@@ -1,15 +1,35 @@
-﻿using SharpDX;
-using SharpDX.DXGI;
+﻿// Copyright (c) 2010-2013 SharpDX - Alexandre Mutel
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SharpDX;
+using SharpDX.DXGI;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using HnadWriting.ComonDX;
 
-namespace HnadWriting.ComonDX
+namespace CommonDX
 {
     /// <summary>
     /// Target to render to a <see cref="SwapChainBackgroundPanel"/>.
@@ -83,7 +103,7 @@ namespace HnadWriting.ComonDX
         protected override SharpDX.DXGI.SwapChain1 CreateSwapChain(SharpDX.DXGI.Factory2 factory, SharpDX.Direct3D11.Device1 device, SharpDX.DXGI.SwapChainDescription1 desc)
         {
             // Creates the swap chain for XAML composition
-            var swapChain = factory.CreateSwapChainForComposition(device, ref desc, null);
+            var swapChain = new SwapChain1(factory, device, ref desc);
 
             // Associate the SwapChainBackgroundPanel with the swap chain
             nativePanel.SwapChain = swapChain;
