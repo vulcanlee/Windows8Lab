0070ce506ea6bc00b26b7400f453c3cee3ecde11
Vulcan Lee <vulcan.lee@gmail.com>
2014-11-08 11:38:33 +0800

    客製化按鈕，可以有三種動畫狀態

---
+++ Source/客製有動畫的按鈕/客製有動畫的按鈕/客製有動畫的按鈕.Windows/button3Img.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace 客製有動畫的按鈕
+{
+    class Button3Img:Button
+    {
+        Image ig = new Image();
+
+        public ImageSource NormalImage
+        {
+            get { return (ImageSource)GetValue(NormalImageProperty); }
+            set { SetValue(NormalImageProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for NormalImage.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty NormalImageProperty =
+            DependencyProperty.Register("NormalImage", typeof(ImageSource), typeof(Button3Img), new PropertyMetadata(0));
+
+        public ImageSource HoverImage
+        {
+            get { return (ImageSource)GetValue(HoverImageProperty); }
+            set { SetValue(HoverImageProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for HoverImage.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HoverImageProperty =
+            DependencyProperty.Register("HoverImage", typeof(ImageSource), typeof(Button3Img), new PropertyMetadata(0));
+
+
+        public ImageSource PressImage
+        {
+            get { return (ImageSource)GetValue(PressImageProperty); }
+            set { SetValue(PressImageProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for PressImage.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PressImageProperty =
+            DependencyProperty.Register("PressImage", typeof(ImageSource), typeof(Button3Img), new PropertyMetadata(0));
+
+
+
+        public double IconWidth
+        {
+            get { return (double)GetValue(IconWidthProperty); }
+            set { SetValue(IconWidthProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IconWidth.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconWidthProperty =
+            DependencyProperty.Register("IconWidth", typeof(double), typeof(Button3Img), new PropertyMetadata(0));
+
+
+
+        public double IconHeight
+        {
+            get { return (double)GetValue(IconHeightProperty); }
+            set { SetValue(IconHeightProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IconHeight.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconHeightProperty =
+            DependencyProperty.Register("IconHeight", typeof(double), typeof(Button3Img), new PropertyMetadata(0));
+
+        
+        
+    }
+}
