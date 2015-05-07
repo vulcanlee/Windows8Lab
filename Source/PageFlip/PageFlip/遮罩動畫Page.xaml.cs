1b518759cb7670b6dde48558e0de627b273038f5
Vulcan Lee <vulcan.lee@gmail.com>
2015-05-07 17:27:49 +0800

    使用Path讓翻頁動畫效果可以呈現由左上到右下

--- Source/PageFlip/PageFlip/遮罩動畫Page.xaml.cs
+++ Source/PageFlip/PageFlip/遮罩動畫Page.xaml.cs
@@ -44,6 +44,9 @@ namespace PageFlip
 
         public void Reset頁面動畫()
         {
+            //LineSegment LineSegment = new LineSegment();
+            //LineSegment.Point = new Point();
+            //LineSegment.Point.
         }
 
         private void backButton_Click(object sender, RoutedEventArgs e)
@@ -51,212 +54,66 @@ namespace PageFlip
             this.Frame.GoBack();
         }
 
-        private async void btnStart由左向右_Click(object sender, RoutedEventArgs e)
+        private async void btnStart由左向右翻頁_Click(object sender, RoutedEventArgs e)
         {
-            PageIndex -= 2;
-            if (PageIndex <= -1)
+            PageIndex += 2;
+            if (PageIndex >= 11)
             {
-                PageIndex = 10;
+                PageIndex = 0;
             }
 
+            gd左邊手寫畫板翻頁動畫.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+            gd右邊手寫畫板翻頁動畫.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
 
-            img左邊手寫畫板翻頁動畫.Source = img左邊手寫畫板原始圖片.Source;
 
-            gd左邊手寫畫板翻頁動畫.Visibility = Windows.UI.Xaml.Visibility.Visible;
-            gd左邊手寫畫板.Visibility = Windows.UI.Xaml.Visibility.Visible;
-            pp左邊手寫畫板翻頁動畫.CenterOfRotationX = 1.0;
 
+
+
+            ibsh左邊手寫畫板翻頁動畫.ImageSource = img左邊手寫畫板原始圖片.Source;
+            ibsh右邊手寫畫板翻頁動畫.ImageSource = img右邊手寫畫板原始圖片.Source;
             img左邊手寫畫板原始圖片.Source = new BitmapImage(new Uri(string.Format(PageImageUrl, PageIndex), UriKind.RelativeOrAbsolute));
-
-            img右邊手寫畫板翻頁動畫.Source = new BitmapImage(new Uri(string.Format(PageImageUrl, PageIndex  + 1), UriKind.RelativeOrAbsolute));
-
-            gd右邊手寫畫板翻頁動畫.Visibility = Windows.UI.Xaml.Visibility.Visible;
-            gd右邊手寫畫板.Visibility = Windows.UI.Xaml.Visibility.Visible;
-            pp右邊手寫畫板翻頁動畫.CenterOfRotationX = 0;
-
-            Reset頁面動畫();
-
-            gd左邊手寫畫板翻頁動畫PPRY_From.Value = 0;
-            gd左邊手寫畫板翻頁動畫PPRY_To.Value = -69;
-            gd右邊手寫畫板翻頁動畫PPRY_From.Value = 69;
-            gd右邊手寫畫板翻頁動畫PPRY_To.Value = 0;
-
-            TurnPage由左往右.Begin();
-            await TurnPage由左往右1.BeginAsync();
-
-            Reset頁面動畫();
-
-            img右邊手寫畫板原始圖片.Source = img右邊手寫畫板翻頁動畫.Source;
-
-            gd左邊手寫畫板翻頁動畫.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
-            gd右邊手寫畫板翻頁動畫.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
-            //img左邊手寫畫板原始圖片
-        }
-
-        private async void btnStart由左向右1_Click(object sender, RoutedEventArgs e)
-        {
-            PageIndex -= 2;
-            if (PageIndex <= -1)
-            {
-                PageIndex = 10;
-            }
-
-            //PageIndex += 2;
-            //if (PageIndex >= 11)
-            //{
-            //    PageIndex = 0;
-            //}
-
-
-            img左邊手寫畫板翻頁動畫.Source = img左邊手寫畫板原始圖片.Source;
-            gd左邊手寫畫板翻頁動畫.Visibility = Windows.UI.Xaml.Visibility.Visible;
-            gd左邊手寫畫板.Visibility = Windows.UI.Xaml.Visibility.Visible;
-            pp左邊手寫畫板翻頁動畫.CenterOfRotationX = 1.0;
-            img左邊手寫畫板原始圖片.Source = new BitmapImage(new Uri(string.Format(PageImageUrl, PageIndex ), UriKind.RelativeOrAbsolute));
-
-            Reset頁面動畫();
-
-            gd左邊手寫畫板翻頁動畫PPRY_From.Value = 0;
-            gd左邊手寫畫板翻頁動畫PPRY_To.Value = -69;
-
-            await TurnPage由左往右.BeginAsync();
-            gd左邊手寫畫板翻頁動畫.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
-
-            img右邊手寫畫板翻頁動畫.Source = new BitmapImage(new Uri(string.Format(PageImageUrl, PageIndex  + 1), UriKind.RelativeOrAbsolute));
-            gd右邊手寫畫板翻頁動畫.Visibility = Windows.UI.Xaml.Visibility.Visible;
-            gd右邊手寫畫板.Visibility = Windows.UI.Xaml.Visibility.Visible;
-            pp右邊手寫畫板翻頁動畫.CenterOfRotationX = 0;
-
-            gd右邊手寫畫板翻頁動畫PPRY_From.Value = 69;
-            gd右邊手寫畫板翻頁動畫PPRY_To.Value = 0;
-
-            await TurnPage由左往右1.BeginAsync();
-
-            Reset頁面動畫();
-
-            img右邊手寫畫板原始圖片.Source = img右邊手寫畫板翻頁動畫.Source;
-
-            gd右邊手寫畫板翻頁動畫.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
-
-        }
-
-        private async void btnStart由右向左_Click(object sender, RoutedEventArgs e)
-        {
-            PageIndex += 2;
-
-            if (PageIndex >= 11)
-            {
-                PageIndex = 0;
-            }
-            //PageIndex -= 2;
-            //if (PageIndex <= -1)
-            //{
-            //    PageIndex = 10;
-            //}
-
-
-
-
-
-
-
-            img右邊手寫畫板翻頁動畫.Source = img右邊手寫畫板原始圖片.Source;
-            gd右邊手寫畫板翻頁動畫.Visibility = Windows.UI.Xaml.Visibility.Visible;
-            gd右邊手寫畫板.Visibility = Windows.UI.Xaml.Visibility.Visible;
             img右邊手寫畫板原始圖片.Source = new BitmapImage(new Uri(string.Format(PageImageUrl, PageIndex + 1), UriKind.RelativeOrAbsolute));
-            pp右邊手寫畫板翻頁動畫.CenterOfRotationX = 0;
-            pp右邊手寫畫板翻頁動畫.RotationY = 0;
-
-            gd右邊手寫畫板翻頁動畫PPRY_From.Value = 0;
-            gd右邊手寫畫板翻頁動畫PPRY_To.Value = 69;
+            await Task.Delay(30);
 
 
-            Reset頁面動畫();
+            ls左邊Point1.Point = new Point(0, 0);
+            ls左邊Point2.Point = new Point(0, 0);
+            ls左邊Point3.Point = new Point(0, 1024);
+            ls左邊Point10.Point = new Point(0, 0);
+            ls左邊Point20.Point = new Point(0, 0);
+            ls左邊Point30.Point = new Point(0, 1024);
+            gd左邊斜角動畫.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            ls右邊Point1.Point = new Point(0, 0);
+            ls右邊Point2.Point = new Point(0, 0);
+            ls右邊Point3.Point = new Point(0, 1024);
+            ls右邊Point10.Point = new Point(0, 0);
+            ls右邊Point20.Point = new Point(0, 0);
+            ls右邊Point30.Point = new Point(0, 1024);
+            gd右邊斜角動畫.Visibility = Windows.UI.Xaml.Visibility.Visible;
 
-            //img右邊手寫畫板原始圖片.Source = img右邊手寫畫板翻頁動畫.Source;
+            var foosbd1 = Storyboard左頁斜角翻頁.BeginAsync();
+            var foosbd2 = Storyboard左頁背景斜角翻頁.BeginAsync();
+            var foosbd3 = Storyboard右頁斜角翻頁.BeginAsync();
+            var foosbd4 = Storyboard右頁背景斜角翻頁.BeginAsync();
+            await Task.WhenAll(foosbd1, foosbd2, foosbd3, foosbd4);
 
-
-
-
-
-            pp左邊手寫畫板翻頁動畫.CenterOfRotationX = 1.0;
-            pp左邊手寫畫板翻頁動畫.RotationY = -69;
-            img左邊手寫畫板翻頁動畫.Source = new BitmapImage(new Uri(string.Format(PageImageUrl, PageIndex), UriKind.RelativeOrAbsolute));
-            gd左邊手寫畫板翻頁動畫.Visibility = Windows.UI.Xaml.Visibility.Visible;
-            gd左邊手寫畫板.Visibility = Windows.UI.Xaml.Visibility.Visible;
-            //
-
-            Reset頁面動畫();
-
-            gd左邊手寫畫板翻頁動畫PPRY_From.Value = -69;
-            gd左邊手寫畫板翻頁動畫PPRY_To.Value = 0;
-
-             TurnPage由左往右1.Begin();
-            await TurnPage由左往右.BeginAsync();
-
-            gd右邊手寫畫板翻頁動畫.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
-            img左邊手寫畫板原始圖片.Source = img左邊手寫畫板翻頁動畫.Source;
-            gd左邊手寫畫板翻頁動畫.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+            gd左邊斜角動畫.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+            gd右邊斜角動畫.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+            ls左邊Point1.Point = new Point(0, 0);
+            ls左邊Point2.Point = new Point(0, 0);
+            ls左邊Point3.Point = new Point(0, 1024);
+            ls左邊Point10.Point = new Point(0, 0);
+            ls左邊Point20.Point = new Point(0, 0);
+            ls左邊Point30.Point = new Point(0, 1024);
+            ls右邊Point1.Point = new Point(0, 0);
+            ls右邊Point2.Point = new Point(0, 0);
+            ls右邊Point3.Point = new Point(0, 1024);
+            ls右邊Point10.Point = new Point(0, 0);
+            ls右邊Point20.Point = new Point(0, 0);
+            ls右邊Point30.Point = new Point(0, 1024);
         }
 
-        private async void btnStart由右向左1_Click(object sender, RoutedEventArgs e)
-        {
-            PageIndex += 2;
-
-            if (PageIndex >= 11)
-            {
-                PageIndex = 0;
-            }
-            //PageIndex -= 2;
-            //if (PageIndex <= -1)
-            //{
-            //    PageIndex = 10;
-            //}
-
-
-
-
-
-            img右邊手寫畫板翻頁動畫.Source = img右邊手寫畫板原始圖片.Source;
-            gd右邊手寫畫板翻頁動畫.Visibility = Windows.UI.Xaml.Visibility.Visible;
-            gd右邊手寫畫板.Visibility = Windows.UI.Xaml.Visibility.Visible;
-            img右邊手寫畫板原始圖片.Source = new BitmapImage(new Uri(string.Format(PageImageUrl, PageIndex  + 1), UriKind.RelativeOrAbsolute));
-            pp右邊手寫畫板翻頁動畫.CenterOfRotationX = 0;
-            pp右邊手寫畫板翻頁動畫.RotationY =0;
-
-            gd右邊手寫畫板翻頁動畫PPRY_From.Value = 0;
-            gd右邊手寫畫板翻頁動畫PPRY_To.Value = 69;
-
-            await TurnPage由左往右1.BeginAsync();
-
-            Reset頁面動畫();
-
-            //img右邊手寫畫板原始圖片.Source = img右邊手寫畫板翻頁動畫.Source;
-
-            gd右邊手寫畫板翻頁動畫.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
-
-
-
-
-            pp左邊手寫畫板翻頁動畫.CenterOfRotationX = 1.0;
-            pp左邊手寫畫板翻頁動畫.RotationY = -69;
-            img左邊手寫畫板翻頁動畫.Source = new BitmapImage(new Uri(string.Format(PageImageUrl, PageIndex), UriKind.RelativeOrAbsolute)); 
-            gd左邊手寫畫板翻頁動畫.Visibility = Windows.UI.Xaml.Visibility.Visible;
-            gd左邊手寫畫板.Visibility = Windows.UI.Xaml.Visibility.Visible;
-            //
-
-            Reset頁面動畫();
-
-            gd左邊手寫畫板翻頁動畫PPRY_From.Value = -69;
-            gd左邊手寫畫板翻頁動畫PPRY_To.Value = 0;
-
-            await TurnPage由左往右.BeginAsync();
-            img左邊手寫畫板原始圖片.Source = img左邊手寫畫板翻頁動畫.Source;
-            gd左邊手寫畫板翻頁動畫.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
-
-        }
-
-        private async void btnStart由左向右單頁_Click(object sender, RoutedEventArgs e)
+        private async void btnStart由右向左翻頁_Click(object sender, RoutedEventArgs e)
         {
             PageIndex -= 2;
             if (PageIndex <= -1)
@@ -264,108 +121,86 @@ namespace PageFlip
                 PageIndex = 10;
             }
 
+            gd左邊手寫畫板翻頁動畫.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+            gd右邊手寫畫板翻頁動畫.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
 
-            img左邊手寫畫板翻頁動畫.Source = img左邊手寫畫板原始圖片.Source;
 
-            gd左邊手寫畫板翻頁動畫.Visibility = Windows.UI.Xaml.Visibility.Visible;
-            gd左邊手寫畫板.Visibility = Windows.UI.Xaml.Visibility.Visible;
-            pp左邊手寫畫板翻頁動畫.CenterOfRotationX = 1.0;
 
+
+            //imgTemp右.Source = null;
+            //imgTemp右.Source = img左邊手寫畫板原始圖片.Source;
+            //imgTemp右.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            ls左邊Point1.Point = new Point(768, 1024);
+            ls左邊Point2.Point = new Point(768, 1024);
+            ls左邊Point3.Point = new Point(768, 1024);
+            ls左邊Point10.Point = new Point(768, 1024);
+            ls左邊Point20.Point = new Point(768, 1024);
+            ls左邊Point30.Point = new Point(768, 1024);
+            gd左邊斜角動畫.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            ls右邊Point1.Point = new Point(768, 1024);
+            ls右邊Point2.Point = new Point(768, 1024);
+            ls右邊Point3.Point = new Point(768, 1024);
+            ls右邊Point10.Point = new Point(768, 1024);
+            ls右邊Point20.Point = new Point(768, 1024);
+            ls右邊Point30.Point = new Point(768, 1024);
+            gd右邊斜角動畫.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            //mypath右邊背景.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+            //mypath右邊.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+
+
+
+
+
+            ibsh左邊手寫畫板翻頁動畫.ImageSource = img左邊手寫畫板原始圖片.Source;
+            ibsh右邊手寫畫板翻頁動畫.ImageSource = img右邊手寫畫板原始圖片.Source;
+            await Task.Delay(50);
+            //img左邊手寫畫板原始圖片.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+            //img右邊手寫畫板原始圖片.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
             img左邊手寫畫板原始圖片.Source = new BitmapImage(new Uri(string.Format(PageImageUrl, PageIndex), UriKind.RelativeOrAbsolute));
-
-            img右邊手寫畫板翻頁動畫.Source = new BitmapImage(new Uri(string.Format(PageImageUrl, PageIndex + 1), UriKind.RelativeOrAbsolute));
-
-            gd右邊手寫畫板翻頁動畫.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
-            gd右邊手寫畫板.Visibility = Windows.UI.Xaml.Visibility.Visible;
-            pp右邊手寫畫板翻頁動畫.CenterOfRotationX = 0;
-
-            Reset頁面動畫();
-
-            gd左邊手寫畫板翻頁動畫PPRY_From.Value = 0;
-            gd左邊手寫畫板翻頁動畫PPRY_To.Value = -69;
-            gd右邊手寫畫板翻頁動畫PPRY_From.Value = 69;
-            gd右邊手寫畫板翻頁動畫PPRY_To.Value = 0;
-
-            await TurnPage由左往右.BeginAsync();
-            //await TurnPage由左往右1.BeginAsync();
-
-            Reset頁面動畫();
-
-            img右邊手寫畫板原始圖片.Source = img右邊手寫畫板翻頁動畫.Source;
-
-            gd左邊手寫畫板翻頁動畫.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
-            gd右邊手寫畫板翻頁動畫.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
-        }
-
-        private async void btnStart由右向左單頁_Click(object sender, RoutedEventArgs e)
-        {
-            PageIndex += 2;
-
-            if (PageIndex >= 11)
-            {
-                PageIndex = 0;
-            }
-            //PageIndex -= 2;
-            //if (PageIndex <= -1)
-            //{
-            //    PageIndex = 10;
-            //}
-
-
-
-
-
-
-
-            img右邊手寫畫板翻頁動畫.Source = img右邊手寫畫板原始圖片.Source;
-            gd右邊手寫畫板翻頁動畫.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
-            gd右邊手寫畫板.Visibility = Windows.UI.Xaml.Visibility.Visible;
             img右邊手寫畫板原始圖片.Source = new BitmapImage(new Uri(string.Format(PageImageUrl, PageIndex + 1), UriKind.RelativeOrAbsolute));
-            pp右邊手寫畫板翻頁動畫.CenterOfRotationX = 0;
-            pp右邊手寫畫板翻頁動畫.RotationY = 0;
-
-            gd右邊手寫畫板翻頁動畫PPRY_From.Value = 0;
-            gd右邊手寫畫板翻頁動畫PPRY_To.Value = 69;
 
 
-            Reset頁面動畫();
+            ls左邊Point1.Point = new Point(0, 0);
+            ls左邊Point2.Point = new Point(0, 0);
+            ls左邊Point3.Point = new Point(0, 1024);
+            ls左邊Point10.Point = new Point(0, 0);
+            ls左邊Point20.Point = new Point(0, 0);
+            ls左邊Point30.Point = new Point(0, 1024);
+            gd左邊斜角動畫.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            ls右邊Point1.Point = new Point(0, 0);
+            ls右邊Point2.Point = new Point(0, 0);
+            ls右邊Point3.Point = new Point(0, 1024);
+            ls右邊Point10.Point = new Point(0, 0);
+            ls右邊Point20.Point = new Point(0, 0);
+            ls右邊Point30.Point = new Point(0, 1024);
+            gd右邊斜角動畫.Visibility = Windows.UI.Xaml.Visibility.Visible;
 
-            //img右邊手寫畫板原始圖片.Source = img右邊手寫畫板翻頁動畫.Source;
+            //imgTemp右.Source = null;
+            //imgTemp右.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+            //await Task.Delay(500);
+            gd右邊斜角動畫.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            img左邊手寫畫板原始圖片.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            img右邊手寫畫板原始圖片.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            var foosbd1 = Storyboard左頁斜角翻頁.BeginAsync();
+            var foosbd2 = Storyboard左頁背景斜角翻頁.BeginAsync();
+            var foosbd3 = Storyboard右頁斜角翻頁.BeginAsync();
+            var foosbd4 = Storyboard右頁背景斜角翻頁.BeginAsync();
+            await Task.WhenAll(foosbd1, foosbd2, foosbd3, foosbd4);
 
-
-
-
-
-            pp左邊手寫畫板翻頁動畫.CenterOfRotationX = 1.0;
-            pp左邊手寫畫板翻頁動畫.RotationY = -69;
-            img左邊手寫畫板翻頁動畫.Source = new BitmapImage(new Uri(string.Format(PageImageUrl, PageIndex), UriKind.RelativeOrAbsolute));
-            gd左邊手寫畫板翻頁動畫.Visibility = Windows.UI.Xaml.Visibility.Visible;
-            gd左邊手寫畫板.Visibility = Windows.UI.Xaml.Visibility.Visible;
-            //
-
-            Reset頁面動畫();
-
-            gd左邊手寫畫板翻頁動畫PPRY_From.Value = -69;
-            gd左邊手寫畫板翻頁動畫PPRY_To.Value = 0;
-
-            //await TurnPage由左往右1.BeginAsync();
-            await TurnPage由左往右.BeginAsync();
-
-            gd右邊手寫畫板翻頁動畫.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
-            img左邊手寫畫板原始圖片.Source = img左邊手寫畫板翻頁動畫.Source;
-            gd左邊手寫畫板翻頁動畫.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
-        }
-
-        private void btn動畫時間_Click(object sender, RoutedEventArgs e)
-        {
-            //TimeSpan foots = TimeSpan.FromMilliseconds(Convert.ToDouble(tbkTime.Text) -
-            //    gd左邊手寫畫板翻頁動畫PPRY_To.KeyTime.TimeSpan.Milliseconds);
-            //gd左邊手寫畫板翻頁動畫PPRY_To.KeyTime = TimeSpan.FromMilliseconds(Convert.ToDouble(tbkTime.Text));
-            gd左邊手寫畫板翻頁動畫PPRY_To.KeyTime = TimeSpan.FromMilliseconds(Convert.ToDouble(tbkTime.Text));
-            gd右邊手寫畫板翻頁動畫PPRY_To.KeyTime = TimeSpan.FromMilliseconds(Convert.ToDouble(tbkTime.Text));
-            //gd右邊手寫畫板翻頁動畫PPRY_To.KeyTime.TimeSpan.Add(
-            //    TimeSpan.FromSeconds(Convert.ToDouble(tbkTime.Text) - 
-            //    gd右邊手寫畫板翻頁動畫PPRY_To.KeyTime.TimeSpan.Milliseconds));
+            gd左邊斜角動畫.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+            gd右邊斜角動畫.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+            ls左邊Point1.Point = new Point(768, 1024);
+            ls左邊Point2.Point = new Point(768, 1024);
+            ls左邊Point3.Point = new Point(768, 1024);
+            ls左邊Point10.Point = new Point(768, 1024);
+            ls左邊Point20.Point = new Point(768, 1024);
+            ls左邊Point30.Point = new Point(768, 1024);
+            ls右邊Point1.Point = new Point(768, 1024);
+            ls右邊Point2.Point = new Point(768, 1024);
+            ls右邊Point3.Point = new Point(768, 1024);
+            ls右邊Point10.Point = new Point(768, 1024);
+            ls右邊Point20.Point = new Point(768, 1024);
+            ls右邊Point30.Point = new Point(768, 1024);
         }
     }
 }
